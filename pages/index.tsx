8617a99dcb722c675e90b38f3627e3deb15d2576
Walison Theodoro <theodoro.walison@gmail.com>
2022-11-25 20:21:04 -0300

    fix: add verse and reverse background video

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Welcome: NextPage = () => {
           muted
           className='absolute z-10 w-auto min-w-full min-h-full max-w-none'
         >
-          <source src='dark-bg-video.mp4' type='video/mp4' />
+          <source src='dark-bg-video-verse-reverse.mp4' type='video/mp4' />
           Your browser does not support the video tag.
         </video>
       </main>
