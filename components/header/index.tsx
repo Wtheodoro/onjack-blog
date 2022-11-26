750cd94a6b5bae5e334df640ef5f096e24da74ae
Walison Theodoro <theodoro.walison@gmail.com>
2022-11-25 21:05:22 -0300

    feat: add personal web anchor

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Logo from '../logo'
@@ -10,11 +11,21 @@ const Header = () => {
         md:left-0 md:w-14 md:h-screen
         bg-white px-6 md:px-0 md:py-10
         flex md:flex-col justify-between items-center
-        shadow-lg md:shadow-2xl'
+        shadow-lg md:border-r-[1px] md:border-black/30'
     >
-      <Link href='/' className='md:-rotate-90 transform translate-y-10'>
+      <Link href='/' className='md:-rotate-90 md:transform md:translate-y-10'>
         <Logo size='small' />
       </Link>
+
+      <a href='https://www.walisontsx.com/'>
+        <Image
+          src='/samurai-mask.jpeg'
+          width={52}
+          height={52}
+          alt='link to creator website'
+          className='rounded-full animate-pulse md:animate-bounce'
+        />
+      </a>
     </header>
   )
 }
